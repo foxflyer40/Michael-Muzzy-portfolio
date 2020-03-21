eed79086ca6a8f61d33751ac7ab85cdf24b4f279
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-03-21 16:14:46 -0400

    all pages basic functions

--- client/src/About.js
+++ client/src/About.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import './App.css';
 
+class About extends React.Component {
+   constructor(props) {
+      super(props)
 
-export default function About(props) {
-   return (
-      <div id='aboutContainer' className='page'>
-         <h1>
-            It's all about Michael...<br />
-         </h1>
+      this.state = {
          
-         <div id='aboutContent'>
-            I grew up in Shelburne, Vermont, the middle child of Bill and 
-            Ramona Muzzy, with a brother on each side: Brion(older), and Ray(younger).
+      }
+   }
+
+   render() {
+      return (
+         <div id='aboutPage' >
+            <h1>
+               It's all about Michael...<br />
+            </h1>
+
+            <div id='aboutContent'>
+               <p>
+                  I grew up in Shelburne, Vermont, the middle child of Bill and
+                  Ramona Muzzy.  Mom (a former RN) had a ceramics studio that took up our entire basement with space for 10-15 students, 3 full size kilns and a slip-casting operation.  Dad (a former Mechanic) was an industrial glassblower at IBM.
+            </p>
+               <p>
+                  With a brother on each side: Brion(older), and Ray(younger), I spent my growing up years enjoying school (mostly), playing neighborhood ball, and riding mini-bikes and motorcycles in the woods and meadows behind our home.
+            </p>
+               <p>
+                  I loved to work alongside Dad whenever he was fixing anything, and I discovered in later years the value of the technical skill I absorbed from this.  Often in the late afternoon he would drive over to the Shelburne boat access to "see what the boats were doing".  We would talk about things and he answered questions like, "How come the guage on the dash says 160 deg. is 'Cold' when clearly anything at that temperature is hot?"
+            </p>
+               <p>
+
+               </p>
+            </div>
          </div>
-      </div>
-   )
+      )
+   }
 }
 
 
+export default About

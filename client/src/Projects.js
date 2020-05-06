58b956988bfc87e48403a540c2f5169fe291b960
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-05-06 13:04:26 -0400

    format projects

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -19,63 +19,61 @@ class Projects extends React.Component {
 
       return (
          <div id='projectsPage'>
-
-            <label>
-               <div className='projectLabel'>
-                  Tic Tac Toe
+            <div id='herokuWork'>Click Image to go to App
+               <label>
+                  <div className='projectLabel'>
+                     Tic Tac Toe
                </div>
-               <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href='https://michael-muzzy-tic-tac-toe.herokuapp.com/'>
-                  <img src={(TicTacToe)} alt='Tic-Tac-Toe board'></img>
-               </a>
-            </label>
+                  <a
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href='https://michael-muzzy-tic-tac-toe.herokuapp.com/'>
+                     <img src={(TicTacToe)} alt='Tic-Tac-Toe board'></img>
+                  </a>
+               </label>
 
-            <br />
-
-            <label>
-               <div className='projectLabel'>
-                  Park Burlington 
+               <label>
+                  <div className='projectLabel'>
+                     Park Burlington
                </div>
-               <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href='http://park-burlington.herokuapp.com/'>
-                  <img src={(ParkBTV)} alt='Park Burlington map'></img>
-               </a>
-            </label>
+                  <a
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href='http://park-burlington.herokuapp.com/'>
+                     <img src={(ParkBTV)} alt='Park Burlington map'></img>
+                  </a>
+               </label>
+            </div>
 
-            <br />
+            <div id='githubWork'>Click image to go to GitHub
 
-            <label>
-               <div className='projectLabel'>
-                  Yelpington
+               <label>
+                  <div className='projectLabel'>
+                     Yelpington
                </div>
-               <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href='https://github.com/BurlingtonCodeAcademy/yelpington-foxflyer40'>
-                  <img src={(Yelpington)} alt='Yelpington board'></img>
-               </a>
+                  <a
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href='https://github.com/BurlingtonCodeAcademy/yelpington-foxflyer40'>
+                     <img src={(Yelpington)} alt='Yelpington board'></img>
+                  </a>
 
-            </label>
+               </label>
 
-            {/* <br /> */}
 
-            <label>
-               <div className='projectLabel'>
-                  GeoVermont
+               <label>
+                  <div className='projectLabel'>
+                     GeoVermont
                </div>
-               <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href='https://github.com/BurlingtonCodeAcademy/geo-vermonter-anis-michael'>
-                  <img src={(GeoVermont)} alt='GeoVermont board'></img>
-               </a>
+                  <a
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     href='https://github.com/BurlingtonCodeAcademy/geo-vermonter-anis-michael'>
+                     <img src={(GeoVermont)} alt='GeoVermont board'></img>
+                  </a>
 
-            </label>
-
+               </label>
+            </div>
          </div>
       )
    }

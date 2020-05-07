4fc6216d243af9d662423c24ab7b2c87d973cc41
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-05-07 16:54:42 -0400

    edit about me text

--- client/src/About.js
+++ client/src/About.js
@@ -19,19 +19,19 @@ class About extends React.Component {
 
             <div id='aboutContent'>
                <p>
-                  I am a native Vermonter with a 37-year-old degree in programming. (Champlain College class of 1983 - A.S. Data Processing)  Funny thing is... I never used it.  Til now.
+                  I am a native Vermonter with a 37-year-old degree in programming. (Champlain College class of 1983 - A.S. Data Processing)  Funny thing is... I never used it.  Until now.
                </p>
                <p>
-                  I completed the Boot-Camp at Burlington Code Academy at the end of April, 2020.  Now ready to enter my third career, as a Full Stack Web Developer.
+                  I completed the Boot-Camp at Burlington Code Academy at the end of April, 2020, and am now ready to enter my third career as a Full Stack Web Developer.
                </p>
                <p>
-                  After a career as an ASE certified Master Automotive Technician, working at local repair garages and dealerships, I joined Good News Garage in 1998.
+                  After a career as an ASE certified Master Automotive Technician, working at local repair garages and dealerships, I joined Good News Garage (GNG) in 1998 as a technician in the repair shop.
                </p>
                <p>
-                  I started out in the repair shop, but in 1999 we received a grant through the federal Job Access and Reverse Commute <a id='aboutLink' target="_blank" href='https://www.transit.dot.gov/funding/grants/grant-programs/job-access-and-reverse-commute-program-5316' rel="noopener noreferrer" className='bannerSelector'>(JARC)</a> program to develop an alternative transportation model intended to address transportation challenges experienced by welfare recipients.  I applied for and was hired as the program manager for the new initiative.
+                  In 1999 GNG received a grant through the federal Job Access and Reverse Commute <a id='aboutLink' target="_blank" href='https://www.transit.dot.gov/funding/grants/grant-programs/job-access-and-reverse-commute-program-5316' rel="noopener noreferrer" className='bannerSelector'>(JARC)</a> program to develop an alternative transportation model intended to address transportation challenges experienced by welfare recipients.  I was excited to be promoted to the position of program manager for the new initiative.
                </p>
                <p>
-                  Over the next 7 years we grew this program from inception to a state-wide transportation service helping low-income families transition from state assistance to self-sufficiency.  The program earned recognition as one of the 100 Best Practices for JARC funded projects.
+                  Over the next 7 years we grew this program from inception to a state-wide transportation service providing over 30,000 rides and helping over 400 low-income families per year transition from state assistance to self-sufficiency.  The program earned recognition as one of the 100 Best Practices for JARC funded projects.
                </p>
                <p>
                   From there I went on to hold other positions within Good News Garage and the parent agency Ascentria Care Alliance (formerly Lutheran Social Services of Northern New England), including Program Director; GNG VT, and Director of Operations; GNG New England.

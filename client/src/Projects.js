00381f9d0d7da744144ca25ae1f64333ce693c4a
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-05-09 10:30:53 -0400

    resize

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -24,7 +24,7 @@ class Projects extends React.Component {
                <div className='projectLabel'>
                   Park Burlington
                      <br />
-                     <h5>(BCA capstone project)</h5>
+                     <h6>(BCA capstone project)</h6>
                </div>
                <br />
                <a

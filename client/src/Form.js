c49eb35de3f8314f2bcafbeef42eccca8749e772
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-05-26 09:45:05 -0400

    switch to test form

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,49 +1,91 @@
 import React from 'react';
 
-export const Form = ({ onSubmit }) => {
-  return (
-    <form id='form' onSubmit={onSubmit}>
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
 
-      <div className="form-group">
-        <label className="form-label" htmlFor="name">Name:</label>
+    this.state = {
+      values: {
+        name: "",
+        email: "",
+        message: "",
+      },
+      isSubmitting: false,
+      isError: false
+    };
+  }
+
+  handleInputChange = e =>
+    this.setState({
+      values: { ...this.state.values, [e.target.name]: e.target.value }
+    });
+
+submitForm = e => {
+    e.preventDefault();
+    console.log(this.state.values);
+}
+
+  render() {
+    return (
+      <form id='form' onSubmit={this.submitForm}>
+
+        <div className="form-group">
+          <label className="form-label" htmlFor="name">Name:</label>
+          <br />
+          <input
+            className="form-control"
+            type='name'
+            name='name'
+            id="name"
+            value={this.state.values.name}
+            onChange={this.handleInputChange}
+            title='Name'
+            required
+          />
+        </div>
         <br />
-        <input
-          className="form-control"
-          id="name" />
-      </div>
-      <br />
 
-      <div className="form-group">
-        <label className="form-label" htmlFor="email">Email address:</label>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">Email address:</label>
+          <br />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="name@example.com"
+            name="email"
+            value={this.state.values.email}
+            onChange={this.handleInputChange}
+            title="Email"
+            required
+          />
+        </div>
         <br />
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          placeholder="name@example.com"
-        />
-      </div>
-      <br />
 
-      <div className="form-group">
-        <label className="form-label" htmlFor="message">Message:</label>
+        <div className="form-group">
+          <label className="form-label" htmlFor="message">Message:</label>
+          <br />
+          <textarea
+            rows="1"
+            cols="26"
+            name="message"
+            className="form-control"
+            id="message">
+            value={this.state.values.message}
+            onChange={this.handleInputChange}
+            title="Message"
+            required
+          </textarea>
+        </div>
         <br />
-        <textarea
-          rows="1"
-          cols="26"
-          name="reply"
-          className="form-control"
-          id="message">
-        </textarea>
-      </div>
-      <br />
-      <br />
+        <br />
 
-      <button id='submit-button' type="submit">
-        Submit
+        <button id='submit-button' type="submit">
+          Submit
         </button>
 
-    </form>
-  );
-};
+      </form>
+    );
+  };
+}
 export default Form;

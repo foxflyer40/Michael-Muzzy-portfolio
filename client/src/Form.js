11f5818d457326966ffdeff817f8c05570b02d05
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-05-26 13:43:34 -0400

    reset form

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,94 +1,49 @@
 import React from 'react';
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props)
+export const Form = ({ onSubmit }) => {
+  return (
+    <form id='form' onSubmit={onSubmit}>
 
-    this.state = {
-      values: {
-        name: "",
-        email: "",
-        message: "",
-      },
-      isSubmitting: false,
-      isError: false
-    };
-  }
-
-
-
-  render() {
-    return (
-      <form id='form' onSubmit={this.submitForm}>
-
-        <div className="form-group">
-          <label className="form-label" htmlFor="name">Name:</label>
-          <br />
-          <input
-            className="form-control"
-            type='name'
-            name='name'
-            id="name"
-            value={this.state.values.name}
-            onChange={this.handleInputChange}
-            title='Name'
-            required
-          />
-        </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="name">Name:</label>
         <br />
+        <input
+          className="form-control"
+          id="name" />
+      </div>
+      <br />
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="email">Email address:</label>
-          <br />
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="name@example.com"
-            name="email"
-            value={this.state.values.email}
-            onChange={this.handleInputChange}
-            title="Email"
-            required
-          />
-        </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="email">Email address:</label>
         <br />
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          placeholder="name@example.com"
+        />
+      </div>
+      <br />
 
-        <div className="form-group">
-          <label className="form-label" htmlFor="message">Message:</label>
-          <br />
-          <textarea
-            rows="1"
-            cols="26"
-            name="message"
-            className="form-control"
-            id="message">
-            value={this.state.values.message}
-            onChange={this.handleInputChange}
-            title="Message"
-            required
-          </textarea>
-        </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="message">Message:</label>
         <br />
-        <br />
+        <textarea
+          rows="1"
+          cols="26"
+          name="reply"
+          className="form-control"
+          id="message">
+        </textarea>
+      </div>
+      <br />
+      <br />
 
-        <button id='submit-button' type="submit">
-          Submit
+      <button id='submit-button' type="submit">
+        Submit
         </button>
 
-      </form>
-    );
-  };
-
-  handleInputChange = e =>
-    this.setState({
-      values: { ...this.state.values, [e.target.name]: e.target.value }
-    });
-
-submitForm = e => {
-    e.preventDefault();
-    console.log(this.state.values);
-}
-
-}
+    </form>
+  );
+};
 export default Form;

119cf85e03f77f6666ec8496efe4dfb7922c0236
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-04 11:49:58 -0400

    change /newContact

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const sgMail = require('@sendgrid/mail');
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencode({ extended: false }));
 
-app.post('/newContact', getMessage);
+app.post('foxflyer40.com', getMessage);
 
 // const messageSchema = new mongoose.Schema({
 //    name: String,

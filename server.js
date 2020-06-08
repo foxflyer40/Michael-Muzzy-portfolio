9fd210038fbea364bbaee0a28a0f484c67c93956
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-08 13:22:06 -0400

    add sendgrid email initial send

--- server.js
+++ server.js
@@ -55,6 +55,21 @@ console.log(msg)
 }
 
 
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
+
 app.use(express.static(path.resolve('./client/build')))
 
 app.get('*', (req, res) => {
@@ -64,4 +79,4 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
    console.log('listening ', port)
 })
-
+openssl

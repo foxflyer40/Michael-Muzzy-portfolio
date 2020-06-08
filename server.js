faa7f150d5bd38136e5fc529a0cf99151e99d3f1
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-08 14:39:02 -0400

    delete commented out stale code

--- server.js
+++ server.js
@@ -5,29 +5,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const sgMail = require('@sendgrid/mail');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// mongoose.connect(`mongodb+srv://michaelmuzzy:${process.env.PASSWORD}@contactformrecord-izqt0.mongodb.net/contact?`)
-// "mongodb+srv://michaelmuzzy:${process.env.PASSWORD}@contactformrecord-izqt0.mongodb.net/test?retryWrites=true&w=majority"
 
-// const newDataBase = mongoose.connection;
-
-// newDataBase.on('error', (err)=>{console.log('something went wrong:',err)});
-// newDataBase.once('open', ()=>{console.log('Connected...')});
-
-// app.use(express.static(path.join(_dirname, '/client/build')));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencode({ extended: false }));
 
 app.post('/Formtwo', getMessage);
 
-// const messageSchema = new mongoose.Schema({
-//    name: String,
-//    email: String,
-//    message: String
-// })
-
-// const Message = mongoose.model('Message', messageSchema);
 
 async function getMessage(req,res) {
    let name = req.body.name;
@@ -50,7 +31,7 @@ async function getMessage(req,res) {
       text: `${message}\nFrom: ${name}`
    };
    sgMail.send(msg);
-   // res.type('application/json').send(JSON.stringify({status: 'thank-you'}))
+   res.type('application/json').send(JSON.stringify({status: 'thank-you'}))
 console.log(msg)
 }
 

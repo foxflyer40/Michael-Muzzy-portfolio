eecce548193e5de0996b5b250bf289305b925f6b
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-09 11:01:07 -0400

    remove catch block

--- client/src/Formtwo.js
+++ client/src/Formtwo.js
@@ -44,7 +44,7 @@ class Formtwo extends React.Component {
       this.setState({
         isSubmitting: jsonObj.status
       })
-    }).catch(err => console.error("Error:", err))
+    })
     
     console.log('state = ' + this.state.name)
     console.log('state = ' + this.state.email)

284ef171207ada44123ebce735037102247b67a3
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-09 09:11:25 -0400

    add catch - add bodyParser - remove metnod=POST from withn form

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const sgMail = require('@sendgrid/mail');
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.post('/Formtwo', getMessage);
 

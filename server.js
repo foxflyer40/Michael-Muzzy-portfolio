254865413101e85c71653176e0033ff2e49159d7
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-17 09:05:29 -0400

    delete sendgrid

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
-const sgMail = require('@sendgrid/mail');
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
@@ -13,42 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/App/Formtwo', getMessage);
 
 
-async function getMessage(req, res) {
-   let name = req.body.name;
-   let email = req.body.email;
-   let message = req.body.message;
-   console.log(name)
-   console.log(email)
-   console.log(message)
+      async function getMessage(req, res) {
+         let name = req.body.name;
+         let email = req.body.email;
+         let message = req.body.message;
+         console.log(name)
+         console.log(email)
+         console.log(message)
+      }
 
 
-   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-   const msg = {
-      to: 'foxflyer40@gmail.com',
-      from: email,
-      subject: 'New Contact from foxflyer40.com',
-      text: `${message}\nFrom: ${name}`
-   };
-   sgMail.send(msg)
-      .then(() => { }, error => {
-         console.error(error);
+      app.use(express.static(path.resolve('./client/build')))
 
-         if (error.res) {
-            console.error(error.res.body)
-         }
-      });
-   res.type('application/json').send(JSON.stringify({ status: 'thank-you' }))
-   console.log(msg)
-}
+      app.get('*', (req, res) => {
+         res.sendFile(path.join(__dirname, '/client/build/index.html'))
+      })
 
-
-app.use(express.static(path.resolve('./client/build')))
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '/client/build/index.html'))
-})
-
-app.listen(port, () => {
-   console.log('listening ', port)
-})
+      app.listen(port, () => {
+         console.log('listening ', port)
+      })
 

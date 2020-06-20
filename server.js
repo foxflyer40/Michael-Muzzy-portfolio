27b45096cc18c583f514ca4466a599032fb57d6f
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-20 09:55:30 -0400

    Delete text.txt

---
+++ server.js
@@ -0,0 +1,51 @@
+
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/App/Formtwo', getMessage);
+
+
+async function getMessage(req, res) {
+   let name = req.body.name;
+   let email = req.body.email;
+   let message = req.body.message;
+
+
+
+res.type('application/json').send(JSON.stringify({ status: 'thank-you' }))
+
+}
+
+var API_KEY = process.env.API_KEY;
+var DOMAIN = 'heroku.com';
+var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+const data = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'foxflyer40@gmail.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomeness!'
+};
+
+mailgun.messages().send(data, (error, body) => {
+  console.log(body);
+});
+
+
+app.use(express.static(path.resolve('./client/build')))
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/client/build/index.html'))
+})
+
+app.listen(port, () => {
+   console.log('listening ', port)
+})
+

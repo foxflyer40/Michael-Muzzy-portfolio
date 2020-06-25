784cdfa0c12460f43a28e48fcfc51a32ee9738be
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-25 15:38:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,20 +18,20 @@ async function getMessage(req, res) {
    let email = req.body.email;
    let message = req.body.message;
 
-   const api_key = process.env.API_KEY
-const DOMAIN = 'heroku.com';
-const mg = mailgun({apiKey: api_key, domain: DOMAIN});
-const data = {
-	from: 'Excited User <me@samples.mailgun.org>',
-	to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
-	subject: 'Hello',
-	text: 'Testing some Mailgun awesomness!'
-};
-mg.messages().send(data, function (error, body) {
-	console.log(body);
-});
+   // const api_key = process.env.API_KEY
+   // const DOMAIN = 'heroku.com';
+   // const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
+   // const data = {
+   //    from: 'Excited User <me@samples.mailgun.org>',
+   //    to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+   //    subject: 'Hello',
+   //    text: 'Testing some Mailgun awesomness!'
+   // };
+   // mg.messages().send(data, function (error, body) {
+   //    console.log(body);
+   // });
 
-res.type('application/json').send(JSON.stringify({ status: 'Your message has been sent.  Thank you!' }))
+   res.type('application/json').send(JSON.stringify({ status: 'Your message has been sent.  Thank you!' }))
 
 }
 

56637ff8c0730b8db08c4d6d81d0b14ce2910f75
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-25 15:27:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const mailgun = require("mailgun-js");
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
@@ -17,26 +18,26 @@ async function getMessage(req, res) {
    let email = req.body.email;
    let message = req.body.message;
 
-
+   const api_key = process.env.API_KEY
+const DOMAIN = 'heroku.com';
+const mg = mailgun({apiKey: api_key, domain: DOMAIN});
+const data = {
+	from: 'Excited User <me@samples.mailgun.org>',
+	to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+	subject: 'Hello',
+	text: 'Testing some Mailgun awesomness!'
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
 
 res.type('application/json').send(JSON.stringify({ status: 'Your message has been sent.  Thank you!' }))
 
 }
 
-var API_KEY = (process.env.API_KEY);
-var DOMAIN = 'heroku.com';
-var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
-const data = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'foxflyer40@gmail.com',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!'
-};
 
-mailgun.messages().send(data, (error, body) => {
-  console.log(body);
-});
+
 
 
 app.use(express.static(path.resolve('./client/build')))

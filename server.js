ac0854f26bf9e6af6ced7d315bdde05344368cc3
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-06-30 15:07:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ async function getMessage(req, res) {
    let message = req.body.message;
 
    // const api_key = process.env.API_KEY
-   // const DOMAIN = 'heroku.com';
+   // const DOMAIN = 'foxflyer40.com';
    // const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
    // const data = {
    //    from: 'Excited User <me@samples.mailgun.org>',
